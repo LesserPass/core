f65d57f7604218f3559acc9da882da3cada663bd
Simon <72569478+Xeimyn@users.noreply.github.com>
2026-04-21 09:48:12 +0200

    init

--- tests/core.js
+++ tests/core.js
@@ -1,2 +1,2 @@
-import { LPCore } from "../core/core.js";
+import { LPCore } from "../core.js";
 // This is where i need to test if output changes with same inputs yk but im SUPER lazy rn so im just writing this comment instead
